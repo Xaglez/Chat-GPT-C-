8c21503754a3811719243c8c5adab805454a4dd2
Xaglez <paulomontealtomac@gmail.com>
2024-02-26 11:37:39 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿class Programa
+{
+    public static void Main()
+    {
+        Stack<int> ints = new Stack<int>();
+        ints.Push(2);
+        ints.Push(3);
+        Console.WriteLine(ints.Last());
+        
+    }
+}
